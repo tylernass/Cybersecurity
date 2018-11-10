39f27871d63dce8023d8e7f05d0a2f745101afa0
Tyler Nass <tyler.nass30@gmail.com>
2018-11-10 12:36:19 -0600

    Update prt.py

--- Network Testing Tool/prt.py
+++ Network Testing Tool/prt.py
@@ -1,54 +1,81 @@
+from threading import Thread
 import tkinter
+import sys
+import tkinter as tk
+from tkinter import ttk
 import socket
-
 open = []
 counting_close = []
 threads = []
 NORM_FONT = ("Helvetica", 10)
 
+# def function():
+#     def hello(OPm, IPm):
+#        print('Great success!')
+#        popup = tk.Tk()
+#        popup.wm_title("Network Report")
+#        label = ttk.Label(popup, text=IPm, font =NORM_FONT)
+#        label.pack(side="top", fill="x", pady="10")
+#        label = ttk.Label(popup, text=OPm, font=NORM_FONT)
+#        label.pack(side="top", fill="x", pady="10")
+#        popup.mainloop()
+#        sys.exit()
+#
+#
+#     def scan(port):
+#         s = socket.socket()
+#         result = s.connect_ex((host, port))
+#         if result == 0:
+#             open.append(port)
+#             s.close()
+#         else:
+#             counting_close.append(port)
+#             s.close()
+#
+#
+#     for i in range(0, 4096):
+#         t=Thread(target=scan(i))
+#         threads.append(t)
+#         t.start()
+
+
 def man():
     top = tkinter.Tk()
-
-    IP = socket.gethostbyname(socket.gethostname())
-    IPm = "IP Address is: " + str(IP)
-    OPm = "Open ports are: " + str(threads)
+    print(open)
+    host = socket.gethostbyname(socket.gethostname())
+    OPm = "Open ports are: " + str(open)
     # remoteServerIP = socket.gethostbyname(IP)
 
-    def hello():
-       print('Great success!')
-       popup = tk.Tk()
-       popup.wm_title("Network Report")
-       label = ttk.Label(popup, text=OPm, font=NORM_FONT)
-       label.pack(side="top", fill="x", pady="10")
-       popup.mainloop()
-       sys.exit()
+    def function():
+        def hello(OPm, IPm):
+            print('Great success!')
+            popup = tk.Tk()
+            popup.wm_title("Network Report")
+            label = ttk.Label(popup, text=IPm, font=NORM_FONT)
+            label.pack(side="top", fill="x", pady="10")
+            label = ttk.Label(popup, text=OPm, font=NORM_FONT)
+            label.pack(side="top", fill="x", pady="10")
+            popup.mainloop()
+            sys.exit()
 
-    def scan(port):
-        s = socket.socket()
-        result = s.connect_ex((IP, port))
-        if result == 0:
-            open.append(port)
-            s.close()
-        else:
-            counting_close.append(port)
+        def scan(port):
+            s = socket.socket()
+            result = s.connect_ex((host, port))
+            if result == 0:
+                open.append(port)
+                s.close()
+            else:
+                counting_close.append(port)
+                s.close()
 
+        print(str(open))
+        for i in range(0, 4096):
+            t = Thread(target=scan(i))
+            threads.append(t)
+            t.start()
 
-
-
-    # def button():
-    #     hello(IPm)
-    #     # for i in range(0, 1024):
-    #     #     t=Thread(target=scan(i))
-    #     #     threads.append(t)
-    #     #     t.start()
-    #     # OPm = "Open port are: " + str(open)
-    #     # print(OPm)
-    #     # hello(OPm, IPm)
-
-
-    B1 = tkinter.Button(top, text = "Scan for open ports", command = hello)
+    B1 = tkinter.Button(top, text="Run Port Scan", command=function)
     B1.pack()
 
-    top.mainloop()
-
-man()
+if __name__ == '__main__':
+    man()
