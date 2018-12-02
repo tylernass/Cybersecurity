77d3d467fb4595be4009a69236bfdbe592c20d42
Tyler Nass <tyler.nass30@gmail.com>
2018-12-01 18:55:52 -0600

    Update distiller.py

--- Dr. Brewster Research in Networking Security/distiller.py
+++ Dr. Brewster Research in Networking Security/distiller.py
@@ -1,6 +1,8 @@
 params = [] # from json-transforms file
 
 list = []
+ln=[]
+paren = []
 
 ftag = [] # list with tag
 fline = [] # list with line numbers
@@ -8,11 +10,66 @@ fline = [] # list with line numbers
 final = []
 i=k=0
 
-# Check for line in ftag
-
-
 
 check = None
+
+x = 0
+
+def chck(line, i, j, k):
+    z = line[(j+1): (k)]
+    # for c in range(0, len(b)+1):
+    #     if c == '}' or c == ',':
+    #         print(c)
+    #         end += 1
+
+    # if end == 0:
+    #     print(b)
+    # print(k)
+    # y = a[(j+1): (k)]
+    # print(z)
+    # print(y)
+
+    for a in range(0, len(ftag)):
+        # print(a)
+        if z == ftag[a]:
+            check = 1
+        # else:
+        #     check = 0
+        a += 1
+    return check
+
+def iter(line, i, j):
+    for k in range(j+1, len(line)):
+        if line[k] == '"':
+            chck(line, i, j, k)
+            if(chck(line, i, j, k) == 1):
+                fline.append(line[0:len(line)-1])
+                # print(fline[k])
+                # print(line[(j+1): (k)])
+                # print('bad')
+                break
+            # if(chck(line, i, j, k) == 2):
+            #     fline.append(line[0:len(line) - 2])
+            # Detect line near end of object
+            else:
+                break
+    # for k in range(0, len(line)):
+    #     if line[k] == '}':
+    #         print('1')
+
+
+with open('5432-3pkts.json', 'r') as g:
+    lines = g.readlines()
+    for i in range(0, len(lines)):
+        line = lines[i]
+
+        ln.append(lines[i])
+        for j in range(0, len(line)):
+            # if line[j] == '}':
+            #     print(i)
+            if line[j] == '"':
+                iter(line, i, j)
+                break
 
 with open('json-transforms.csv', 'r') as f:
     lines = f.readlines()
@@ -27,48 +84,6 @@ with open('json-transforms.csv', 'r') as f:
                 j += 1
     f.close()
 
-def iter(line, j):
-    for k in range(j+1, len(line)):
-        if line[k] == '"':
-            chck(line, j, k)
-            if(chck(line, j, k) == 1):
-                fline.append(line[0:len(line)-1])
-                # print(fline[k])
-                # print(line[(j+1): (k)])
-                # print('bad')
-                break
-            else:
-                break
-
-
-# Do all of the lines get passed in as they are supposed to? Yes
-def chck(line, j, k):
-    z = line[(j+1): (k)]
-    # b = line[(k): (k)+1]
-    # print(b)
-    print(z)
-    check = 0
-    ###### THIS IS WHERE THE ERROR IS ####
-    for a in range(0, len(ftag)):
-        # print(a)
-        # Where the error should be handled
-        if z == ftag[a]:
-            check = 1
-        # else:
-        #     check = 0
-        a += 1
-    return check
-
-with open('5432-3pkts.json', 'r') as g:
-    lines = g.readlines()
-    for i in range(0, len(lines)):
-        line = lines[i]
-        # print(line)
-        for j in range(0, len(line)):
-            if line[j] == '"':
-                iter(line, j)
-                break
-
 a=0
 for x in range(0, len(params)):
     for y in range(0, len(list)):
@@ -76,8 +91,12 @@ for x in range(0, len(params)):
             a += 1
 
 with open('Corrected-5432--3pkts-out.json', 'w') as f:
+    print(fline)
     for i in range(0, len(fline)):
         # Opening Brackets
+        print(fline[i])
+        if fline[i][0:] == '}' or ',':
+            print(fline[i][0:])
         if fline[i][6:11] == "index":
             f.write("{\n")
             f.write(fline[i] + "\n")
@@ -90,35 +109,3 @@ with open('Corrected-5432--3pkts-out.json', 'w') as f:
             f.write(fline[i] + "\n")
 
     f.close()
-
-# with open('json-transforms.csv', 'r') as f:
-#     lines = f.readlines()
-#     # for line in lines:
-#     for i in range(1, len(lines)):
-#         line = lines[i]
-#         for j in range(0, len(line)):
-#             if line[j] == ',':
-#                 params.append(line[:j])
-#                 break
-#             else:
-#                 j += 1
-            # elif line[j] == ',':
-            #     params.append(line[:j])
-            #     print(i)
-    # f.close()
-
-
-# print(params)
-    # line = lines
-    # for i in range(0, len(params)):
-    #     for x in range(0, len(list)):
-    #         y = file[list[x] - 2]
-    #         z = len(y)
-    #         a = y[z - 2]
-    #         # if x == 0:
-    #         #     b = 2
-    #         # else: b = list[x]
-    #         # print(list[x])
-    #         if a == comma:
-    #             ntxt = file[list[x] - 2][0:(z - 2)]
-    #             file[list[x] - 2] = ntxt
