5ed8d4ef6f7ab949abc169863693f3b75677cab4
Tyler Nass <tyler.nass30@gmail.com>
2018-12-07 16:14:41 -0600

    Update dlt.py

--- Dr. Franklin Research in Data Science/dlt.py
+++ Dr. Franklin Research in Data Science/dlt.py
@@ -1,11 +1,6 @@
 # Description: Script to delete repeat files in a directory
 
-# NOTE: To be run after files have been 1. Migrated (see migrate script) and 2.
-import os
-# loc = "/tartarus/tylernass/"
-# Needs to be recursive
-for i in os.listdir(:
-    print(i)
-    for j in os.i:
-        if j[-3] == "zip":
-            os.remove('%s' % i)
+def dlt():
+    for i in os.listdir():
+        if i[-3:] == "zip":
+            os.remove(i)
