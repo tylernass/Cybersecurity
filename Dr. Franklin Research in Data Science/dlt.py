dc4ff5b29bda221e274b6823253c2e770da375c6
Tyler Nass <tyler.nass30@gmail.com>
2018-12-08 21:13:14 -0600

    Delete dlt.py

--- Dr. Franklin Research in Data Science/dlt.py
+++
@@ -1,8 +0,0 @@
-# Description: Script to delete repeat files in a directory
-loc = "/tartarus/tylernass" # NOTE: This is the location of the script, for every nth directory
-# Example location "/tartarus/tylernass/1", "/tartarus/tylernass/2", ... "/tartarus/tylernass/n"
-
-def dlt():
-    for i in os.listdir():
-        if i[-3:] == "zip":
-            os.remove(i) 
