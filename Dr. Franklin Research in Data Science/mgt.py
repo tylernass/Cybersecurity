c7329b350e956547c6ab115d7d6ee166ccf46eaf
Tyler Nass <tyler.nass30@gmail.com>
2018-12-08 18:31:48 -0600

    Update mgt.py

--- Dr. Franklin Research in Data Science/mgt.py
+++ Dr. Franklin Research in Data Science/mgt.py
@@ -1,17 +1,22 @@
 # Description: Used to migrate ~25 .zip files from a central repository to an individual repository
 
+# MIGRATE SCRIPT
 import os
 loc = "/tartarus/tylernass"
 nw = "/tartarus/DATASETS/SmartMeterData"
 
+a = -
 def migrate(loc):
+    global a
     for i in os.listdir():
         loc = loc + str(i)
         shutil.move("/tartarus/DATASETS/SmartMeterData", loc)
-
-def crtdir(nw):
-    a=0
-    for i in range(0, 25):
         a += 1
-        os.mkdir(nw + str(a))
+     crtdir(loc)
+
+# CREATE DIRECTORY SCRIPT
+def crtdir(loc):
+    global a
+    for i in range(0, a):
+        os.mkdir(loc + str(a))
 
